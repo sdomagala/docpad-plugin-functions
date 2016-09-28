4bd3d6a9750982fe46da65e7a43bcdb0081e446c
Domagala, Sebastian <sebastian.domagala@sap.com>
2016-09-28 09:27:23 +0200

    initial

--- es2015/index.js
+++ es2015/index.js
@@ -4,43 +4,35 @@ var _async = require('async');
 
 var _async2 = _interopRequireDefault(_async);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _extender = require('./extender.js');
 
-var extend = function extend(child, parent) {
-  for (var key in parent) {
-    if (hasProp.call(parent, key)) child[key] = parent[key];
-  }function ctor() {
-    this.constructor = child;
-  }ctor.prototype = parent.prototype;child.prototype = new ctor();child.__super__ = parent.prototype;return child;
-},
-    hasProp = {}.hasOwnProperty;
+var _extender2 = _interopRequireDefault(_extender);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function (BasePlugin) {
 
-  var FunctionPlugin = void 0;
+  function FunctionPlugin(opts) {
+    var docpad = opts.docpad;
+    this.createEventHandlers(docpad);
+    FunctionPlugin.__super__.constructor.apply(this, arguments);
+  }
 
-  return FunctionPlugin = function (BasePlugin) {
+  (0, _extender2.default)(FunctionPlugin, BasePlugin);
 
-    function FunctionPlugin(opts) {
-      var docpad = opts.docpad;
-      this.createEventHandlers(docpad);
-      GulpPlugin.__super__.constructor.apply(this, arguments);
-    }
+  FunctionPlugin.prototype.name = 'functions';
 
-    extend(FunctionPlugin, BasePlugin);
-
-    FunctionPlugin.prototype.name = 'functions';
-
-    FunctionPlugin.prototype.createEventHandlers = function (docpad) {
-      docpad.getEvents().forEach(function (_this) {
-        return function (eventName) {
-          return _this[eventName] = function (opts, next) {
-            var tasks = _this.getConfig()[eventName];
-            tasks ? _async2.default.series(tasks, next) : next();
-          };
+  FunctionPlugin.prototype.createEventHandlers = function (docpad) {
+    docpad.getEvents().forEach(function (_this) {
+      return function (eventName) {
+        return _this[eventName] = function (opts, next) {
+          var tasks = _this.getConfig()[eventName];
+          tasks ? _async2.default.series(tasks, next) : next();
         };
-      }(this));
-      return this;
-    };
-  }(BasePlugin);
+      };
+    }(this));
+    return this;
+  };
+
+  return FunctionPlugin;
 };
--- source/index.js
+++ source/index.js
@@ -1,34 +1,29 @@
 import async from 'async';
-
-var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-  hasProp = {}.hasOwnProperty;
+import extend from './extender.js';
 
 module.exports = function (BasePlugin) {
 
-  let FunctionPlugin;
+  function FunctionPlugin(opts) {
+    const docpad = opts.docpad;
+    this.createEventHandlers(docpad);
+    FunctionPlugin.__super__.constructor.apply(this, arguments);
+  }
 
-  return FunctionPlugin = (function(BasePlugin) {
+  extend(FunctionPlugin, BasePlugin);
 
-    function FunctionPlugin(opts) {
-      const docpad = opts.docpad;
-      this.createEventHandlers(docpad);
-      GulpPlugin.__super__.constructor.apply(this, arguments);
-    }
+  FunctionPlugin.prototype.name = 'functions';
 
-    extend(FunctionPlugin, BasePlugin);
-
-    FunctionPlugin.prototype.name = 'functions';
-
-    FunctionPlugin.prototype.createEventHandlers = function(docpad) {
-      docpad.getEvents().forEach((function(_this) {
-        return function(eventName) {
-          return _this[eventName] = function(opts, next) {
-            const tasks = _this.getConfig()[eventName];
-            tasks ? async.series(tasks, next) : next();
-          };
+  FunctionPlugin.prototype.createEventHandlers = function(docpad) {
+    docpad.getEvents().forEach((function(_this) {
+      return function(eventName) {
+        return _this[eventName] = function(opts, next) {
+          const tasks = _this.getConfig()[eventName];
+          tasks ? async.series(tasks, next) : next();
         };
-      })(this));
-      return this;
-    };
-  })(BasePlugin);
+      };
+    })(this));
+    return this;
+  };
+
+  return FunctionPlugin;
 };
