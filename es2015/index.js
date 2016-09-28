9f7103848e8d00e9118d6558a7f1a527ac6a84ef
Domagala, Sebastian <sebastian.domagala@sap.com>
2016-09-28 08:59:15 +0200

    initial

--- es2015/index.js
+++ es2015/index.js
@@ -1,53 +1,45 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _async = require('async');
 
 var _async2 = _interopRequireDefault(_async);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+var extend = function extend(child, parent) {
+  for (var key in parent) {
+    if (hasProp.call(parent, key)) child[key] = parent[key];
+  }function ctor() {
+    this.constructor = child;
+  }ctor.prototype = parent.prototype;child.prototype = new ctor();child.__super__ = parent.prototype;return child;
+},
+    hasProp = {}.hasOwnProperty;
 
 module.exports = function (BasePlugin) {
 
-  return function (_BasePlugin) {
-    _inherits(BaseClass, _BasePlugin);
+  var FunctionPlugin = function (BasePlugin) {
+    extend(FunctionPlugin, BasePlugin);
 
-    function BaseClass(opts) {
-      _classCallCheck(this, BaseClass);
+    FunctionPlugin.prototype.name = 'functions';
 
+    FunctionPlugin.prototype.createEventHandlers = function (docpad) {
+      docpad.getEvents().forEach(function (_this) {
+        return function (eventName) {
+          return _this[eventName] = function (opts, next) {
+            var tasks = _this.getConfig()[eventName];
+            tasks ? _async2.default.series(tasks, next) : next();
+          };
+        };
+      }(this));
+      return this;
+    };
+
+    function FunctionPlugin(opts) {
       var docpad = opts.docpad;
-
-      var _this = _possibleConstructorReturn(this, (BaseClass.__proto__ || Object.getPrototypeOf(BaseClass)).call(this, opts));
-
-      _this.createEventHandlers(docpad);
-      return _this;
+      this.createEventHandlers(docpad);
+      GulpPlugin.__super__.constructor.apply(this, arguments);
     }
-
-    _createClass(BaseClass, [{
-      key: 'attachEvents',
-      value: function attachEvents(func) {
-        func.call(this);
-      }
-    }, {
-      key: 'docpadReady',
-      value: function docpadReady(opts, next) {
-        var tasks = this.getConfig()['docpadReady'];
-        tasks ? _async2.default.series(tasks, next) : next();
-      }
-    }, {
-      key: 'name',
-      get: function get() {
-        return 'functions';
-      }
-    }]);
-
-    return BaseClass;
   }(BasePlugin);
+
+  return FunctionPlugin;
 };
--- source/index.js
+++ source/index.js
@@ -1,27 +1,33 @@
 import async from 'async';
 
+var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
+
 module.exports = function (BasePlugin) {
 
+  const FunctionPlugin = (function(BasePlugin) {
+    extend(FunctionPlugin, BasePlugin);
 
-  return class BaseClass extends BasePlugin {
+    FunctionPlugin.prototype.name = 'functions';
 
-    constructor(opts) {
-      const { docpad } = opts;
-      super(opts);
+    FunctionPlugin.prototype.createEventHandlers = function(docpad) {
+      docpad.getEvents().forEach((function(_this) {
+        return function(eventName) {
+          return _this[eventName] = function(opts, next) {
+            const tasks = _this.getConfig()[eventName];
+            tasks ? async.series(tasks, next) : next();
+          };
+        };
+      })(this));
+      return this;
+    };
+
+    function FunctionPlugin(opts) {
+      const docpad = opts.docpad;
       this.createEventHandlers(docpad);
+      GulpPlugin.__super__.constructor.apply(this, arguments);
     }
+  })(BasePlugin);
 
-    get name () {
-      return 'functions';
-    }
-
-    attachEvents(func) {
-      func.call(this);
-    }
-
-    docpadReady (opts, next) {
-      const tasks = this.getConfig()['docpadReady'];
-      tasks ? async.series(tasks, next) : next();
-    }
-  };
+  return FunctionPlugin;
 };
